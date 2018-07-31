1569867f20d6b1de1a1866c509ac044221c550fc
Adam Hall <adamhalldesigns@gmail.com>
2018-07-31 12:39:56 +0100

    refactored solution leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,23 +1,26 @@
-// trying to get into teh ramda way of things
+// trying to get into the ramda way of things
 const R = require('ramda');
 
 const divisibleBy = R.curry((x, y) => R.modulo(y, x) === 0);
 const notDivisibleBy = R.complement(divisibleBy); // TODO i'd prefer something on the fly on line 16
 
-var Year = function (year) {
+const isLeap = R.both(
+    divisibleBy(4),
+    R.either(
+        notDivisibleBy(100),
+        R.both(
+            divisibleBy(100),
+            divisibleBy(400),
+        ),
+    ),
+);
+
+const Year = function (year) {
     this.year = year;
 };
 
 Year.prototype.isLeap = function () {
-    return R.both(
-        divisibleBy(4),
-        R.either(
-            notDivisibleBy(100),
-            R.both(
-                divisibleBy(100),
-                divisibleBy(400)),
-        ),
-    )(this.year)
+    return isLeap(this.year);
 };
 
 module.exports = Year;

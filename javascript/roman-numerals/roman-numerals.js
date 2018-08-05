195021f70a8777af2760aefcf4c482469d7dfaad
Adam Hall <adamhalldesigns@gmail.com>
2018-08-05 21:17:55 +0100

    handles 4's

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,23 +1,29 @@
-const translate = [
+const translation = Object.freeze([
     ['M', 1000],
+    ['CM', 900],
     ['D', 500],
+    ['CD', 400],
     ['C', 100],
+    ['XC', 90],
     ['L', 50],
+    ['XL', 40],
     ['X', 10],
+    ['IX', 9],
     ['V', 5],
+    ['IV', 4],
     ['I', 1],
-];
+]);
 
-const toRoman = num => {
-    return translate.reduce(([remainder, roman], [letter, value]) => {
-        if (value > remainder) return [remainder, roman];
+const arraySizeOf = (v, l) => new Array(Math.floor(v)).fill(l).join('');
 
-        const number = Math.floor(remainder / value);
-        return [
-            (remainder % value),
-            roman.concat(new Array(number).fill(letter).join('')),
-        ]
-    }, [num, ''])[1];
-}
+const toRoman = num => translation
+    .reduce(([remainder, romanValue], [romanNumeral, value]) => (
+        value > remainder
+            ? [remainder, romanValue]
+            : [
+                remainder % value,
+                romanValue.concat(arraySizeOf((remainder / value), romanNumeral)),
+            ]
+    ), [num, ''])[1];
 
 module.exports = toRoman;

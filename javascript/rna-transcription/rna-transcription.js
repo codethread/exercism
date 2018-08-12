6db71a894a43fe1707fa5a5a74242aeb8069eb01
Adam Hall <adamhalldesigns@gmail.com>
2018-08-12 14:28:29 +0100

    rna in a ramda style

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,26 +1,26 @@
 const R = require('ramda');
 
 function DnaTranscriber() {
-    const rel = Object.freeze({
+    const inputs = {
         C: 'G',
         G: 'C',
         A: 'U',
         T: 'A',
-    });
-
-    const valid = (x) => {
-        if (rel[x]) return x;
-        throw new Error('Invalid input');
     };
 
-    const transcribe = R.prop(R.__, rel);
+    const throwIfInvalid = options => (input) => {
+        if (R.not(R.has(input, options))) throw new Error('Invalid input');
+    };
+
+    const transcribeIfValid = R.pipe(
+        R.tap(throwIfInvalid(inputs)),
+        R.prop(R.__, inputs),
+    );
 
     return {
         toRna: R.pipe(
             R.split(''),
-            R.map(
-                R.when(valid, transcribe),
-            ),
+            R.map(transcribeIfValid),
             R.join(''),
         ),
     };

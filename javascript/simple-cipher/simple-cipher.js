62c726580da039c022e2045cfdea1838123a1593
Adam Hall <adamhalldesigns@gmail.com>
2018-08-23 23:20:21 +0100

    getting there

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,7 +1,54 @@
+function validCipher(key) {
+  if (!key) return null;
+}
+
 class Cipher {
-  static key() {
-    return (Math.random() + 1).toString(36).substring(7);
+  constructor(cipher) {
+    this.possible = "abcdefghijklmnopqrstuvwxyz";
+    this.randL = this.randL.bind(this);
+    this.key = validCipher(cipher) || [...Array(100)].map(this.randL).join("");
+
+    this.toNum = this.toNum.bind(this);
+    this.asNumbers = this.key.split("").map(this.toNum);
+
+    this.advance = this.advance.bind(this);
+    this.difference = this.difference.bind(this);
+  }
+  randL() {
+    return this.possible.charAt(
+      Math.floor(Math.random() * this.possible.length)
+    );
+  }
+
+  toNum(letter) {
+    return this.possible.indexOf(letter);
+  }
+
+  advance(letter, pos) {
+    const double = this.possible + this.possible;
+    return double[this.asNumbers[pos] + this.possible.indexOf(letter)];
+  }
+
+  difference(letter, pos) {
+    const double = this.possible + this.possible;
+    return double[
+      this.possible.indexOf(letter) - this.possible.indexOf(this.key[pos])
+    ];
+  }
+
+  encode(sub) {
+    return sub
+      .split("")
+      .map(this.advance)
+      .join("");
+  }
+
+  decode(sub) {
+    return sub
+      .split("")
+      .map(this.difference)
+      .join("");
   }
 }
 
-module.exports = Cipher;
+export default Cipher;
